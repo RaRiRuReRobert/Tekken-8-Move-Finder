b4866a5acf0d2e56938e5905c98a484dc90fe55a
Robert Battle <rbattle6199@gmail.com>
2024-08-20 15:39:35 -0400

    Web scrape any character's move frame data

--- main.py
+++ main.py
@@ -27,40 +27,70 @@ move_input = [frames.getText().strip(f"{character}-")for frames in soup.find_all
 
 result_frames = []
 result_inputs = []
-
-print(move_startup)
-print(move_input)
-#Reductive method for narrowing down which moves to display
-for frames in move_startup:
-    frame_index = move_startup.index(frames)
-    if frames == "":
-        frame_data = 1000
-    elif "~" in frames:
-        frame_data = int(frames[:2])
-    else:
-        frame_data = int(frames)
-
-    if frame_data == int(frame_search):
-        result_frames.append(move_startup[frame_index])
-        result_inputs.append(move_input[frame_index])
-        del move_startup[frame_index]
-        del move_input[frame_index]
-
-print(result_frames)
-print(result_inputs)
-
-##Formatting##
-data = {
-    "input": "",
-    "startup frames": "",
-}
-
 all_inputs_and_frames = []
 
-#null frames set to 1000
+#print(move_startup)
+#print(move_input)
 
-#if frame_search =/<= int(scraped start up frames):
-##Add new dictionary to list of dictionaries including:
-###{input: scraped text
-###frames: scraped number}
+#Reductive method for narrowing down which moves to display\
 
+
+def exact_frames():
+    global move_startup
+    global move_input
+    global all_inputs_and_frames
+    for frames in move_startup:
+        frame_index = move_startup.index(frames)
+        if frames == "":
+            frame_data = 1000
+        elif "~" in frames:
+            frame_data = int(frames[:2])
+        else:
+            frame_data = int(frames)
+
+        if frame_data == int(frame_search):
+            result_frames.append(move_startup[frame_index])
+            result_inputs.append(move_input[frame_index])
+            data = {
+                "input":move_input[frame_index],
+                "startup frames": move_startup[frame_index],
+            }
+            all_inputs_and_frames.append(data)
+            del move_startup[frame_index]
+            del move_input[frame_index]
+
+
+def fewer_frames():
+    global move_startup
+    global move_input
+    global all_inputs_and_frames
+    for frames in move_startup:
+        frame_index = move_startup.index(frames)
+        if frames == "":
+            frame_data = 1000
+        elif "~" in frames:
+            frame_data = int(frames[:2])
+        else:
+            frame_data = int(frames)
+
+        if frame_data <= int(frame_search):
+            result_frames.append(move_startup[frame_index])
+            result_inputs.append(move_input[frame_index])
+            data = {
+                "input": move_input[frame_index],
+                "startup frames": move_startup[frame_index],
+            }
+            all_inputs_and_frames.append(data)
+            del move_startup[frame_index]
+            del move_input[frame_index]
+#print(result_frames)
+#print(result_inputs)
+
+if include_below == "Yes":
+    fewer_frames()
+    print(all_inputs_and_frames)
+elif include_below == "No":
+    exact_frames()
+    print(all_inputs_and_frames)
+else:
+    print("Invalid input, please rerun program")
